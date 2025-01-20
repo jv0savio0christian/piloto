2cb2ddbe3fb4c3ff87495663e4368750d17c29e0
andinniv <avgc2411@gmail.com>
2025-01-20 11:46:51 -0300

    vercel

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def sobre(request):
+    return render(request,'sobre.html')
+def contato(request):
+    return render(request,'contato.html')
+def ajuda(request):
+    return render(request,'ajuda.html')
